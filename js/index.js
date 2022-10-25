60c518040e8d525accbc3a6d7aa0d8f73470c79b
LucasCandiago <lucas.candiago@gmail.com>
2022-10-25 13:31:14 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    nav: false,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
